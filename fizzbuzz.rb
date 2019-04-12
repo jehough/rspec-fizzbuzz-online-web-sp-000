155a7265bf684b71a7ae09bd2c67a4a11759ba64
Jim Hough <teachlife@outlook.com>
2019-04-12 15:23:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz (n)
-  if n % 3 == 0
-    "Fizz"
-  end
-  elsif n % 5 == 0
-    puts "Buzz"
-  end
-  elsif n % 15 == 0
-    puts "FizzBuzz"
-  end
-  else
-    NIL
-  end
+def fizzbuzz (int)
+  if int % 3 == 0
+  "Fizz"
 end
     
